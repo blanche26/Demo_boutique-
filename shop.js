4294f5faf5f280fc5be805142bbb1710f382c915
blanche26 <lystolepadiolekamaleulatale@gmail.com>
2026-06-02 17:19:45 +0100

    Update shop.js

--- shop.js
+++ shop.js
@@ -88,6 +88,102 @@ function mettreAJourLeRendu() {
         corpsTableau.innerHTML += '<tr>' +
             '<td>' + item.name + '</td>' +
             '<td>' + formatPrice(item.price) + '</td>' +
+            '<td>' + item.qvar produits = [];
+var panier = [];
+
+window.onload = function() {
+    chargerProduits();
+    var barre = document.getElementById("barre-recherche");
+    if (barre) {
+        barre.oninput = function() {
+            var saisie = barre.value.toLowerCase();
+            var resultatFiltre = [];
+            for (var i = 0; i < produits.length; i++) {
+                if (produits[i].name.toLowerCase().indexOf(saisie) !== -1) {
+                    resultatFiltre.push(produits[i]);
+                }
+            }
+            afficherLeCatalogue(resultatFiltre);
+        };
+    }
+};
+
+function chargerProduits() {
+    fetch("products.json")
+        .then(function(reponse) { return reponse.json(); })
+        .then(function(donnees) {
+            produits = donnees;
+            afficherLeCatalogue(produits);
+        });
+}
+
+function formatPrice(prix) {
+    return prix + " XAF";
+}
+
+function afficherLeCatalogue(liste) {
+    var zoneCatalogue = document.getElementById("charge_produit");
+    if (!zoneCatalogue) return;
+    
+    zoneCatalogue.innerHTML = "";
+    
+    for (var i = 0; i < liste.length; i++) {
+        var p = liste[i];
+        zoneCatalogue.innerHTML += '<div class="carte-produit">' +
+            '<h3>' + p.name + '</h3>' +
+            '<p>Catégorie : ' + p.category + '</p>' +
+            '<p>Prix : ' + formatPrice(p.price) + '</p>' +
+            '<button onclick="ajouterAuPanier(' + p.id + ')">Ajouter</button>' +
+        '</div>';
+    }
+}
+
+// --- VOTRE PANIER (INTOUCHÉ ET SÉCURISÉ) ---
+
+function ajouterAuPanier(idDuProduit) {
+    var prodSelectionne = null;
+    for (var i = 0; i < produits.length; i++) {
+        if (produits[i].id == idDuProduit) {
+            prodSelectionne = produits[i];
+        }
+    }
+    
+    var produitExiste = null;
+    for (var j = 0; j < panier.length; j++) {
+        if (panier[j].id == idDuProduit) {
+            produitExiste = panier[j];
+        }
+    }
+    
+    if (produitExiste != null) {
+        produitExiste.quantite++;
+    } else {
+        panier.push({
+            id: prodSelectionne.id,
+            name: prodSelectionne.name,
+            price: prodSelectionne.price,
+            quantite: 1
+        });
+    }
+    mettreAJourLeRendu();
+}
+
+function mettreAJourLeRendu() {
+    var corpsTableau = document.getElementById("lignes-du-panier");
+    var zoneTotal = document.getElementById("total-panier");
+    if (!corpsTableau) return;
+    
+    corpsTableau.innerHTML = "";
+    var calculTotal = 0;
+    
+    for (var i = 0; i < panier.length; i++) {
+        var item = panier[i];
+        var sousTotal = item.price * item.quantite;
+        calculTotal = calculTotal + sousTotal;
+        
+        corpsTableau.innerHTML += '<tr>' +
+            '<td>' + item.name + '</td>' +
+            '<td>' + formatPrice(item.price) + '</td>' +
             '<td>' + item.quantite + '</td>' +
             '<td>' + formatPrice(sousTotal) + '</td>' +
             '<td>' +
@@ -96,9 +192,7 @@ function mettreAJourLeRendu() {
             '</td>' +
         '</tr>';
     }
-    if (zoneTotal) {
-        zoneTotal.textContent = formatPrice(calculTotal);
-    }
+    if (zoneTotal) zoneTotal.textContent = formatPrice(calculTotal);
 }
 
 function modifierQuantite(idDuProduit, valeur) {
